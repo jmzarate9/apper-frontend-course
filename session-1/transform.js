1f193588de22e2c86163f64af5559487dcd0a114
Joshua Zarate <joshua.zarate@gcash.com>
2023-07-05 01:02:41 +0800

    Updated transform.js

--- session-1/transform.js
+++ session-1/transform.js
@@ -2,7 +2,7 @@
 // Homework #1
 
 function transform(nums) {
-    let squaredNums = nums.map(number => number ** 2);
+    let squaredNums = nums.map(number => number*number);
     //The sort() sorts the elements of an array.
         //array.sort(compareFunction)
             //When sort() compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
